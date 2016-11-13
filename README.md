f7b9e878f9533f74f6e3b7b2b7599f7a63cb86bf
Caden Jiang <jiangshen16@icloud.com>
2016-11-12 22:58:36 -0500

    initial android studio project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ProductFinder
+# CometGuide
---
+++ app/src/main/java/gtappathon/cometguide/ProductList.java
@@ -0,0 +1,13 @@
+package gtappathon.cometguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProductList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+    }
+}
