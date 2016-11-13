a9b8e35cfecad66e9fc96db646451483399646f3
Caden Jiang <jiangshen16@icloud.com>
2016-11-12 23:18:50 -0500

    all good

--- app/src/main/java/gtappathon/cometguide/ProductList.java
+++ app/src/main/java/gtappathon/cometguide/ProductList.java
@@ -1,9 +1,9 @@
 package gtappathon.cometguide;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ProductList extends Activity {
+public class ProductList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
