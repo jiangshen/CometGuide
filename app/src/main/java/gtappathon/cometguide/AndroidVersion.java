fbfb0cd71b9d0de0bd3c102d57d85a036905f56d
Caden Jiang <jiangshen16@icloud.com>
2016-11-13 00:34:54 -0500

    finished recycler grid

---
+++ app/src/main/java/gtappathon/cometguide/AndroidVersion.java
@@ -0,0 +1,26 @@
+package gtappathon.cometguide;
+
+/**
+ * Created by jiangshen on 11/13/16.
+ */
+
+public class AndroidVersion {
+    private String recyclerViewTitleText;
+    private int recyclerViewImage;
+
+    public String getrecyclerViewTitleText() {
+        return recyclerViewTitleText;
+    }
+
+    public void setAndroidVersionName(String recyclerVietTitleText) {
+        this.recyclerViewTitleText = recyclerVietTitleText;
+    }
+
+    public int getrecyclerViewImage() {
+        return recyclerViewImage;
+    }
+
+    public void setrecyclerViewImage(int recyclerViewImage) {
+        this.recyclerViewImage = recyclerViewImage;
+    }
+}
--- app/src/main/java/gtappathon/cometguide/ProductList.java
+++ app/src/main/java/gtappathon/cometguide/ProductList.java
@@ -2,12 +2,97 @@ package gtappathon.cometguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ProductList extends AppCompatActivity {
+
+    private final String recyclerViewTitleText[] = {"Android", "RecyclerView", "Android List", "GridView", "ListView", "Tutorial", "Example", "CardView", "Lollipop", "Marshmallow", "Custom ListView", "Custom GridView"
+    };
+
+    private final int recyclerViewImages[] = {
+            R.drawable.tools, R.drawable.tools, R.drawable.tools, R.drawable.tools, R.drawable.tools, R.drawable.tools,
+            R.drawable.tools, R.drawable.tools, R.drawable.tools, R.drawable.tools, R.drawable.tools, R.drawable.tools
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+        initRecyclerViews();
+    }
+
+    private void initRecyclerViews() {
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        ArrayList<AndroidVersion> av = prepareData();
+        AndroidDataAdapter mAdapter = new AndroidDataAdapter(getApplicationContext(), av);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(getApplicationContext(), new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int i) {
+                        switch (i) {
+                            case 0:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 1:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 2:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 3:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 4:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 5:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 6:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 7:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 8:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 9:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 10:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                            case 11:
+                                Toast.makeText(view.getContext(), "position= " + i, Toast.LENGTH_LONG).show();
+                                break;
+                        }
+                    }
+                })
+        );
+
+    }
+
+    private ArrayList<AndroidVersion> prepareData() {
+
+        ArrayList<AndroidVersion> av = new ArrayList<>();
+        for (int i = 0; i < recyclerViewTitleText.length; i++) {
+            AndroidVersion mAndroidVersion = new AndroidVersion();
+            mAndroidVersion.setAndroidVersionName(recyclerViewTitleText[i]);
+            mAndroidVersion.setrecyclerViewImage(recyclerViewImages[i]);
+            av.add(mAndroidVersion);
+        }
+        return av;
     }
 }
